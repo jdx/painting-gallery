bd898d9406ac66da83e8176afa5368a1c4a72efe
Jeff Dickey <dickeytk@gmail.com>
2011-07-09 20:01:10 -0700

    Initial commit, some work done already

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaintingGallery::Application.config.secret_token = '84cff5e7fede9a72042fa48e98ae8205c58f31a673f6fb67b11621e7143aa32efc56a7a6beba1cf05eeff121df6ea86f072b3bf8b63010aacc5b21e4f7dd820b'
